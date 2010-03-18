fa45e390a2a63663bd4948da262398baa868c8a8
Dan Drinkard <dan.drinkard@gmail.com>
2010-03-18 11:47:43 -0400

    add 2 levels of subdirs inside cache file structure

--- config/cache.php
+++ config/cache.php
@@ -1,7 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['cache_dir'] = APPPATH.'cache/';
+$config['cache_dir'] = BASEPATH.'cache/partials/';
 
-$config['cache_default_expires'] = 0;
+$config['cache_default_expires'] = 600; // 10 minutes
 
 ?>
